4ad86d3ba108272385f0ea31918c95ff7e91cc24
mmuu1987 <262663941@qq.com>
2020-07-13 18:09:15 +0800

    update

--- Assets/Scripts/Setting/SettingManager.cs
+++ Assets/Scripts/Setting/SettingManager.cs
@@ -295,24 +295,30 @@ public class Setting
 
     public Setting()
     {
-        FirstDir = Application.streamingAssetsPath + "/大事记/2001-2009";
-        SecondDir = Application.streamingAssetsPath + "/大事记/2010-2019";
-        ThirdDir = Application.streamingAssetsPath + "/大事记/2020";
+        string path = Application.dataPath;//
+        
+        DirectoryInfo dif = new DirectoryInfo(path);
 
-        IcOne = Application.streamingAssetsPath + "/公司介绍/集团介绍";
-        IcTwo = Application.streamingAssetsPath + "/公司介绍/基本信息";
-        IcThree = Application.streamingAssetsPath + "/公司介绍/股东概况";
-        IcFour = Application.streamingAssetsPath + "/公司介绍/荣誉奖项";
-        IcFive = Application.streamingAssetsPath + "/公司介绍/产品体系";
-        IcSix = Application.streamingAssetsPath + "/公司介绍/服务体系";
+        DirectoryInfo temp = dif.Parent.Parent;//得到了母程序StreamingAssets文件夹所在的路径
 
-        PhOne = Application.streamingAssetsPath + "/私享传家/品牌介绍";
-        PhTwo = Application.streamingAssetsPath + "/私享传家/尊享服务";
-        PhThree = Application.streamingAssetsPath + "/私享传家/大湾区高净值中心";
+        FirstDir = temp + "/大事记/2001-2009";
+        SecondDir = temp + "/大事记/2010-2019";
+        ThirdDir = temp + "/大事记/2020";
 
-        OsOne = Application.streamingAssetsPath + "/卓越风采/MDRT荣誉榜";
-        OsTwo = Application.streamingAssetsPath + "/卓越风采/2020年MDRT达标榜";
-        OsThree = Application.streamingAssetsPath + "/卓越风采/双百万储备力量";
+        IcOne = temp + "/公司介绍/集团介绍";
+        IcTwo = temp + "/公司介绍/基本信息";
+        IcThree = temp + "/公司介绍/股东概况";
+        IcFour = temp + "/公司介绍/荣誉奖项";
+        IcFive = temp + "/公司介绍/产品体系";
+        IcSix = temp + "/公司介绍/服务体系";
+
+        PhOne = temp + "/私享传家/品牌介绍";
+        PhTwo = temp + "/私享传家/尊享服务";
+        PhThree = temp + "/私享传家/大湾区高净值中心";
+
+        OsOne = temp + "/卓越风采/MDRT荣誉榜";
+        OsTwo = temp + "/卓越风采/2020年MDRT达标榜";
+        OsThree = temp + "/卓越风采/双百万储备力量";
     }
 
 
